0134a1076277d0d2e8ac072a91749d0c93ef9baf
Kuldeep Singh Bhandari <singh.kuldeepbhandari@gmail.com>
2021-03-27 23:55:25 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'; 
-import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
