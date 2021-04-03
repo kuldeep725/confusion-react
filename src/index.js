3003813cd474f00d5c8f7d70964222f823b0c799
Kuldeep Singh Bhandari <singh.kuldeepbhandari@gmail.com>
2021-04-03 13:22:42 +0530

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// importing css
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
+import './App.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
